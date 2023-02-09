900201f629a65640ff9e5ec8b2ae1044199040bc
jinho0114 <tuonm123@naver.com>
2023-02-09 18:06:00 +0900

    0209

---
+++ src/main/java/Plant/Plant_Project/PlantProjectApplication.java
@@ -0,0 +1,13 @@
+package Plant.Plant_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantProjectApplication.class, args);
+	}
+
+}
