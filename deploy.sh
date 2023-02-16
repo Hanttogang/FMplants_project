6b7dcb4508a51af81cc6eec9d76b7ed1f607c937
jinho0114 <118782940+jinho0114@users.noreply.github.com>
2023-02-17 01:01:36 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/plantProject
 cd $REPOSITORY
 
-APP_NAME=action_codedeploy
+APP_NAME=plantProject
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
