a9f5ee8ea59f7dd0c4e9457b0b3a69aba7b3703f
jinho0114 <tuonm123@naver.com>
2023-02-17 00:48:55 +0900

    0209

--- src/main/java/Plant/Plant_Project/Controller/HelloController.java
+++ src/main/java/Plant/Plant_Project/Controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 
     @GetMapping("/api/hello")
     public List<String> Hello(){
-        return Arrays.asList("서버 포트는 8080", "리액트 포트는 3000");
+        return Arrays.asList("테스트테스트테스트테스트");
     }
 }
 
