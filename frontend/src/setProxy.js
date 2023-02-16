8b4312efc8a70a9cff668cee7b70f647b05d24da
jinho0114 <118782940+jinho0114@users.noreply.github.com>
2023-02-16 23:57:46 +0900

    Update setProxy.js

--- frontend/src/setProxy.js
+++ frontend/src/setProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8080',	# 서버 URL or localhost:설정한포트번호
+      target: 'http://15.164.39.86:8080',	# 서버 URL or localhost:설정한포트번호
       changeOrigin: true,
     })
   );
